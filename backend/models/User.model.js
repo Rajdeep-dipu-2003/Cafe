6f4bf92b7aee03057664607b3661bb257118bc8c
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2025-12-28 21:52:03 +0530

    Add role to user model

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
             type: Number,
             required: true,
         },
+        role: {
+            type: String,
+            enum: ["user", "admin"],
+            default: "user"
+        },
         orders: [
             {
                 order: {
