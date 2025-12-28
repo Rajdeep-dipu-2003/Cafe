4ccad99867dba3ead6b67c19701014d80c3f524d
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2025-12-28 22:52:25 +0530

    Add category dto

---
+++ backend/dto/Category.dto.js
@@ -0,0 +1,25 @@
+const { z } = require("zod");
+const HttpException = require("../models/http-exception.js")
+
+const categorySchema = z.object({
+    name: z.string().min(1),
+    imageUrl: z.string().min(1)
+});
+
+class CategoryDto {
+    constructor(data) {
+        Object.assingn(this, data);
+    }
+
+    static fromRequest(body) {
+        try {
+            const parsed = categorySchema.parse(body);
+            return CategoryDto(parsed);
+        }
+        catch(e) {
+            throw HttpException(400, "Invalid or incomplete category feild.");
+        }
+    }
+}
+
+module.exports = CategoryDto
