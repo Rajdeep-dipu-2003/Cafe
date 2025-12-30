6687d1ef666149d2eff56dc5eb7d6b5be9af7056
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2025-12-30 23:25:58 +0530

    Add get category and get product by category end points

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const adminRouter = express.Router();
 
+const authorizeAdmin = require("../middlewares/authorizeAdmin.js")
+
 const AdminController = require("../controllers/admin.controller");
 const adminController = new AdminController();
+
+adminRouter.use(authorizeAdmin);
 
 adminRouter.post("/add-prouduct", async(req, res) => {
     try {
@@ -25,4 +29,31 @@ adminRouter.post("/delete-product", async(req, res) => {
             .json({ error: "Inernal Server Error" })
     }
 });
+
+adminRouter.get("/get-all-products/:category", async (req, res) => {
+    try {        
+        const allProductsOfCategory = await adminController.getAllProductsOfCategory(req, res);
+        
+        // TODO : Add pageing
+        return res.status(200).json({ products : allProductsOfCategory});
+    }
+    catch(e) {
+        res
+            .status(e?.errorCode || 500)
+            .json({ error: e?.message || "Inernal Server Error" })
+    }
+}); 
+
+adminRouter.post("/create-new-category", async (req, res) => {
+    try {
+        await adminController.createNewCategory(req, res);
+
+        return res.status(200).json({ message: "New category created successfully."})
+    }
+    catch(e) {
+        res
+            .status(e?.errorCode || 500)
+            .json({ error: e?.message || "Inernal Server Error" })
+    }
+})
 
