0ca65041fbc69fafe6ba4edf67a480ab9a6e4760
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-03 07:33:10 +0530

    Add user routes

---
+++ backend/routes/user.route.js
@@ -0,0 +1,28 @@
+// TODO : add authentication middleware
+
+const express = require("express");
+const userRouter = express.router();
+
+// routes 
+// - get all categories
+// - get top 10 popular products
+// - get all products of a specific category: 
+
+// getRoutes.get("/get-all-categories", async (req, res) => {
+//     try {
+//         await 
+//     }
+//     catch(e) {
+//         res
+//             .status(e?.errorCode || 500)
+//             .json({ error: e?.message || "Internal server error"})
+//     }
+// })
+
+// getRoutes.get("/get-popular-products", async (req, res) => {
+
+// })
+
+// getRoutes.get("/get-all-products/:category", async (req, res) => {
+
+// })
