e6efbca3452d837967fddf03a8d0f1f34c60fd35
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-04 12:21:57 +0530

    Add getAllProductsOfCategory function inside product services

--- backend/services/product.service.js
+++ backend/services/product.service.js
@@ -34,6 +34,26 @@ async function createNewProduct(productDto) {
     }
 }
 
+async function getAllProductsOfCategory(categoryName) {
+    try {
+        const category = await Category.findOne({ name: categoryName });
+
+        if (!category) {
+            throw new HttpException(400, "No relevant category found.");
+        }
+
+        const products = await Product.find({ category: category._id });
+
+        return products;
+    } catch (e) {
+        const status = e?.errorCode || 500;
+        const errorMessage = e?.message || "Failed to fetch products of the given category.";
+
+        throw new HttpException(status, errorMessage);
+    }
+}
+
 module.exports = {
     createNewProduct,
+    getAllProductsOfCategory
 }
