dab033d2ac3bf75028ecee4e325bfcaec6458431
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-04 10:28:31 +0530

    Pre save hook function here need notbe async

--- backend/models/Category.model.js
+++ backend/models/Category.model.js
@@ -24,7 +24,7 @@ const categorySchema = new mongoose.Schema(
     }
 )
 
-categorySchema.pre("save", async function () {
+categorySchema.pre("save", function () {
     if (this.isModified('name')) {
         this.slug = this.name
             .toLowerCase()
