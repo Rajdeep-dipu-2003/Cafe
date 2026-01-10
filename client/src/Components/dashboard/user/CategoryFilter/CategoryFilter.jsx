0c05af66eea20d8b2f2fc4c270985a6292c759bb
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-10 16:09:52 +0530

    Connect Category filter section to backend.

--- client/src/Components/dashboard/user/CategoryFilter/CategoryFilter.jsx
+++ client/src/Components/dashboard/user/CategoryFilter/CategoryFilter.jsx
@@ -4,19 +4,42 @@ import wrapsImg from "../../../../assets/wraps.avif"
 import burgerImg from "../../../../assets/burger.avif"
 import desertImg from "../../../../assets/desert.avif"
 
+import api from "@lib/axios.js"
+
 import CategoryItem from "./CategoryItem";
+import { useEffect, useState } from "react"
+import toast, { Toaster } from 'react-hot-toast';
 
 function CategoryFilter() {
-  const categories = [
-    { label: "Biryani", image: biryaniImg },
-    { label: "Pizzas", image: pizzaImg },
-    { label: "Wraps", image: wrapsImg },
-    { label: "Burger", image: burgerImg },
-    { label: "Dessert", image: desertImg },
-  ];
+  // const categories = [
+  //   { label: "Biryani", image: biryaniImg },
+  //   { label: "Pizzas", image: pizzaImg },
+  //   { label: "Wraps", image: wrapsImg },
+  //   { label: "Burger", image: burgerImg },
+  //   { label: "Dessert", image: desertImg },
+  // ];
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getAllCategories = async () => {
+      try {
+        const response = await api.get("/shared/get-all-categories");
+        console.log(response.data)
+        setCategories(response.data.categories);
+      }
+      catch (e) {
+        console.error("Error while fetching category details: ", e);
+        toast.error("Failed to get category.\nPlease try again later");
+      }
+    };
+
+    getAllCategories();
+  }, [])
 
   return (
     <section className="w-full px-6 py-6">
+      <Toaster/>
       <h2 className="text-xl font-semibold mb-4 text-center">
         Popular Cuisines
       </h2>
@@ -31,9 +54,9 @@ function CategoryFilter() {
       >
         {categories.map((item) => (
           <CategoryItem
-            key={item.label}
-            image={item.image}
-            label={item.label}
+            key={item.name}
+            image={item.imageUrl}
+            label={item.name}
           />
         ))}
       </div>
