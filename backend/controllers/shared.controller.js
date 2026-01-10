29daf518fd17b35663445e073076ba7927afcfee
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-10 14:09:37 +0530

    Add shared routes and controller for shared resources between the user and the admin

---
+++ backend/controllers/shared.controller.js
@@ -0,0 +1,40 @@
+const categoryService = require("../services/category.service.js")
+const productService = require("../services/product.service.js")
+
+const HttpException = require("../models/http-exception.js")
+
+class SharedController {
+    async getAllCategories(req, res) {
+        try {
+            const allCategories = await categoryService.getAllCategories();
+
+            res.status(200).json({
+                message: "Successfully fetched all categories.",
+                categories: allCategories
+            })
+        }
+        catch (e) {
+            const status = e?.errorCode || 500;
+            const errorMessage = e?.message || "Inernal Server Error";
+
+            throw new HttpException(status, errorMessage);
+        }
+    }
+
+    async getAllProductsOfCategory(req, res) {
+        try {
+            const { categoryName } = req.query;
+            const allProductsOfCategory = await productService.getAllProductsOfCategory(categoryName);
+
+            return res.status(200).json({ products: allProductsOfCategory });
+        }
+        catch (e) {
+            const status = e?.errorCode || 500;
+            const errorMessage = e?.message || "Failed to fetch products of the given category.";
+
+            throw new HttpException(status, errorMessage);
+        }
+    }
+};
+
+module.exports = SharedController
---
+++ backend/routes/shared.routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const sharedRouter = express.Router();
+const SharedController = require("../controllers/shared.controller")
+const sharedController = new SharedController();
+
+const authenticateUser = require("../middlewares/authentication.middleware")
+
+sharedRouter.get("/get-all-categories", async (req, res) => {
+    try {
+        await sharedController.getAllCategories(req, res);
+    }
+    catch (e) {
+        res
+            .status(e?.errorCode || 500)
+            .json({ error: e?.message || "Inernal Server Error" })
+    }
+})
+
+// To get all products of a specific category
+sharedRouter.get("/get-all-products", async (req, res) => {
+    try {
+        await sharedController.getAllProductsOfCategory(req, res);
+    }
+    catch (e) {
+        res
+            .status(e?.errorCode || 500)
+            .json({ error: e?.message || "Inernal Server Error" })
+    }
+});
+
+module.exports = sharedRouter;
