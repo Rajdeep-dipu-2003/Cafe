125d8b4aaa2aba495f882e405425ac3159a9669d
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-13 21:55:33 +0530

    Add cart service with add to cart function.

--- backend/services/cart.service.js
+++ backend/services/cart.service.js
@@ -1,20 +1,44 @@
-const Cart = require("Cart");
+const Cart = require("../models/Cart.model");
+const Product = require("../models/Product.model");
+const HttpException = require("../models/http-exception");
 
 async function addToCart(productId, userId) {
     try {
-        const cart = await Cart.findOne({user : userId});
-        
-        const product = cart.items.find(item => item.productId.toString() === productId);
 
-        if (product) {
-            product.quantity += 1;
+        const productExists = await Product.findOne({ _id: productId });
+
+        if (!productExists) {
+            throw new HttpException(404, "Product not found.");
+        }
+
+        const cart = await Cart.findOne({ user: userId });
+
+        if (!cart) {
+            const cart = {
+                user: userId,
+                items: [
+                    {
+                        productId,
+                        quantity: 1
+                    }
+                ]
+            };
+
+            await Cart.create(cart);
         }
         else {
-            cart.items.push({ productId })
-        }
+            const product = cart.items?.find(item => item.productId.toString() === productId);
 
-        await cart.save();
-0    }
+            if (product) {
+                product.quantity += 1;
+            }
+            else {
+                cart.items.push({ productId })
+            }
+
+            await cart.save();
+        }
+    }
     catch (e) {
         const status = e?.errorCode || 500;
         const message = e?.message || "Error while category creation.";
