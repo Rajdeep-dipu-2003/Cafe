a3709de3e066f879a3429c503caa06c4f544bd75
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-13 21:57:22 +0530

    Add addToCart function to user controller.

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,9 +1,33 @@
-
+const HttpException = require("../models/http-exception");
+const cartService = require("../services/cart.service")
 
 class UserController {
-    async getAllCategories(req, res) {
+    async addToCart(req, res) {
         try {
-            await Category.find({})
+            const { productId, userId } = req.body;
+
+            if (!productId) {
+                throw new HttpException(400, "Invalid Product Id.")
+            }
+
+            if (!userId) {
+                throw new HttpException(403, "Invalid User Id.")
+            }
+
+
+            await cartService.addToCart(productId, userId);
+
+            return res.status(201).json({
+                status: "Success"
+            })
+
+        }
+        catch (e) {
+            res
+                .status(e?.errorCode || 500)
+                .json({ error: e?.message || "Internal Server Error" })
         }
     }
 }
+
+module.exports = UserController
