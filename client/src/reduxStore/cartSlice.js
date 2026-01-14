fd4d18c6f6938ae3707ac363cf7795b9c5ce012d
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-15 00:32:38 +0530

    Configure redux store.

---
+++ client/src/reduxStore/cartSlice.js
@@ -0,0 +1,68 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import api from "@lib/axios"
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        items: []
+    },
+    reducers: {
+        addToCart(state, action) {
+            const product = action.payload;
+
+            const existingItem = state.items.find(
+                item => item._id == product._id
+            );
+
+            if (existingItem) {
+                existingItem.quantity += 1;
+            }
+            else {
+                state.items.push({
+                    ...product, 
+                    quantity : 1
+                });
+            }
+        },
+        removeFromCart(state, action) {
+
+            const product = action.payload;
+
+            const existingItem = state.items.find(
+                item => item._id == product._id
+            );
+
+            if (existingItem.quantity > 1) {
+                existingItem.quantity -= 1;
+            }
+            else {
+                state.items = state.items.filter(
+                    item => item._id !== product._id
+                );
+            }
+        },
+    }
+})
+
+export const addToCartAsync = createAsyncThunk(
+    "cart/addToCartAsync",
+    async(product, { dispatch }) => {
+        dispatch(addToCart(product)); // immediately update the redux store
+
+        // persist in the backend
+        // TODO : update the quantity logic
+        await api.post("/user/add-to-cart", {
+            product,
+            quantity: 1
+        });
+    }
+)
+
+// export const removeFromCartAsync = createAsyncThunk(
+//     "carrt/removeFromCart",
+//     async
+// )
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
