179b984834320d784d51730a676951c0f736558e
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-14 23:35:19 +0530

    Add navigation to cart page.

--- client/src/Components/dashboard/user/Navbar/CartButton.jsx
+++ client/src/Components/dashboard/user/Navbar/CartButton.jsx
@@ -1,8 +1,13 @@
 import { ShoppingCart } from "lucide-react";
+import { useNavigate } from "react-router";
 
 function CartButton() {
+
+  const navigate = useNavigate();
+
   return (
     <button
+      onClick={() => navigate("/user/cart")}
       className="relative flex items-center justify-center p-2 rounded-full 
                  hover:bg-gray-100 transition"
       aria-label="Cart"
