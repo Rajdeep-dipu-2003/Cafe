d5decae7ed1ddc46ba64e21afae9821e66701bee
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-17 11:12:56 +0530

    Add removeFromCart controller function.

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,7 +5,7 @@ class UserController {
     async addToCart(req, res) {
         try {
             const { productId } = req.body;
-            const userId = req.user.userId;
+            const userId = req.user?.userId || "696667622dfe107216821e81";
 
             if (!productId) {
                 throw new HttpException(400, "Invalid Product Id.")
@@ -17,6 +17,34 @@ class UserController {
 
 
             await cartService.addToCart(productId, userId);
+
+            return res.status(201).json({
+                status: "Success"
+            })
+
+        }
+        catch (e) {
+            res
+                .status(e?.errorCode || 500)
+                .json({ error: e?.message || "Internal Server Error" })
+        }
+    }
+
+    async removeFromCart(req, res) {
+         try {
+            const { productId } = req.body;
+            const userId = req.user?.userId || "696667622dfe107216821e81";
+
+            if (!productId) {
+                throw new HttpException(400, "Invalid Product Id.")
+            }
+
+            if (!userId) {
+                throw new HttpException(403, "Invalid User Id.")
+            }
+
+
+            await cartService.removeFromCart(productId, userId);
 
             return res.status(201).json({
                 status: "Success"
