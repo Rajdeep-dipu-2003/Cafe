3bdc9de62a2531e5c9a36a8b58485a3ce2f1a78d
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-18 00:41:44 +0530

    Update zod object to check if price is a numerical string.

--- backend/dto/Product.dto.js
+++ backend/dto/Product.dto.js
@@ -3,11 +3,9 @@ const { z } = require("zod");
 
 const productSchema = z.object({
     name: z.string().min(1),
-    price: z.number().positive(),
+    price: z.coerce.number().positive("Price must be a positive number"),
     categoryId: z.string().min(1),
-    imageUrl: z.string().min(1),
     description: z.string().min(1).max(500),
-    tags: z.array(z.string()).optional()
 });
 
 class ProductDTO {
