047c1140042ce415e3e6bf4eaf922699b8479bd4
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-17 11:18:03 +0530

    Add remove from cart service.

--- backend/services/cart.service.js
+++ backend/services/cart.service.js
@@ -41,7 +41,46 @@ async function addToCart(productId, userId) {
     }
     catch (e) {
         const status = e?.errorCode || 500;
-        const message = e?.message || "Error while category creation.";
+        const message = e?.message || "Error while adding product to cart.";
+        throw new HttpException(status, message);
+    }
+}
+
+async function removeFromCart(productId, userId) {
+    try {
+        const productExists = await Product.findOne({ _id: productId });
+
+        if (!productExists) {
+            throw new HttpException(404, "Product not found.");
+        }
+
+        const cart = await Cart.findOne({ user: userId });
+
+        if (!cart) {
+            throw new HttpException(404, "Cart is empty.")
+        }
+        else {
+            const product = cart.items?.find(item => item.productId.toString() === productId);
+
+            if (product) {
+                product.quantity -= 1;
+            }
+            else {
+                throw new HttpException(404, "Given Product is already removed!")
+            }
+
+            if (product.quantity === 0) {
+                cart.items = cart.items.filter(item => 
+                    item._id.toString() != product._id
+                )
+            }
+
+            await cart.save();
+        }
+    }
+    catch (e) {
+        const status = e?.errorCode || 500;
+        const message = e?.message || "Error while deleting product from cart.";
         throw new HttpException(status, message);
     }
 }
