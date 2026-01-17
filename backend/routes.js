6e3adc0a2cda78840f089c7fdee172ee7892d1af
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-18 00:45:40 +0530

    Configure routes for respective end points of admin, shared, auth and user.

--- backend/routes.js
+++ backend/routes.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const v1_router = express.Router();
 const adminRouter = require("./routes/admin.route")
+const sharedRouter = require("./routes/shared.routes")
+const authRouter = require("./routes/auth.routes")
+const userRouter = require("./routes/user.route")
 
 // v1_router.use("/user", );
 v1_router.use("/admin", adminRouter);
+v1_router.use("/shared", sharedRouter);
+v1_router.use("/auth", authRouter);
+v1_router.use("/user", userRouter);
 
 module.exports = v1_router
 
