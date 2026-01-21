b843138f39d0453376d244fb38254da0fea5b487
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-21 23:45:02 +0530

    Add role specific routing.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,12 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home"
 import UserDashboard from "./Pages/UserDashboard";
+import AdminLogin from "./Components/dashboard/admin/AdminLogin";
+import AdminDashboard from "./Pages/AdminDashboard";
+import ProductView from "./Components/dashboard/user/ProductsView/ProductsView";
+import Cart from "./Components/dashboard/user/Cart/Cart";
+import Authentication from "./Components/dashboard/user/Authentication/Authentication";
+import UserLayout from "./Components/dashboard/user/UserLayout";
 
 function App() {
 
@@ -9,7 +15,19 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/user-dashboard" element={<UserDashboard/>}></Route>
+
+        <Route path="/admin">
+          <Route path="login" element={<AdminLogin/>}/>
+          <Route path="dashboard" element={<AdminDashboard/>}/>
+        </Route>
+
+        <Route path="/user" element={<UserLayout/>}>
+          <Route path="dashboard" element={<UserDashboard/>}/>
+          <Route path="view-products" element={<ProductView/>}/>
+          <Route path="cart" element={<Cart/>}/>
+          <Route path="authenticate" element={<Authentication/>}/>
+        </Route>
+
       </Routes>
     </>
   )
