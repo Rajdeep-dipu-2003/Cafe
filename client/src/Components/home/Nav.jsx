d456bb2bd232208babd36b1f94636f0885fa606b
Rajdeep-dipu-2003 <rajdeepaaron1@gmail.com>
2026-01-30 23:33:11 +0530

    Add routing to navbar.

--- client/src/Components/home/Nav.jsx
+++ client/src/Components/home/Nav.jsx
@@ -1,12 +1,26 @@
+import { useNavigate } from "react-router";
+
 function Nav() {
+  const navigate = useNavigate();
+
+  const handelNavigation = () => {
+    navigate("/user/authenticate")
+  }
+
   return (
     <nav className="fixed top-0 left-0 w-full h-16 bg-white/80 backdrop-blur-md z-50 flex items-center">
       <div className="w-full px-6 flex justify-end items-center gap-4">
-        <button className="text-red-600 font-medium hover:text-red-700 transition">
+        <button
+            onClick={handelNavigation} 
+            className="text-red-600 font-medium hover:text-red-700 transition"
+          >
           Login
         </button>
 
-        <button className="px-4 py-2 rounded-md bg-red-600 text-white font-medium hover:bg-red-700 transition">
+        <button 
+          onClick={handelNavigation} 
+          className="px-4 py-2 rounded-md bg-red-600 text-white font-medium hover:bg-red-700 transition"
+        >
           Sign Up
         </button>
       </div>
